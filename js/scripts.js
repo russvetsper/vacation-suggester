cc36cd9060268c1c3c2a3bf9e72a3c80f722d4f3
russ <russvetsper@gmail.com>
2016-06-17 12:29:43 -0700

    restarted project, added forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,5 @@
 $(document).ready(function() {
-  $("form#insurance").submit(function(event) {
-    var age = parseInt($("input#age").val());
-    var gender = $("select#gender").val();
-
-    var quote = (100 - age) * 3;
-    if (gender === 'male' && age < 26) {
-      quote += 50;
-    }
-
-    $("#pice").text(quote);
-    $("#value").show()
+  
 
     event.preventDefault();
   });
