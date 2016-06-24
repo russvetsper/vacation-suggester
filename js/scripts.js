abdfc9d4e95f84d6ac57b876b4aa2d183ab665a1
Russvetsper <russvetsper@gmail.com>
2016-06-24 15:07:00 -0700

    some fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 $("#serving").submit(function(event){
 var location1 = $("input:radio[name=location1]:checked").val();
- if (location1 === "yes") {
+var location2 = $("input:radio[name=location2]:checked").val();
+var location3 = $("input:radio[name=location3]:checked").val();
+var location4 = $("input:radio[name=location4]:checked").val();
+ if (location1 === "yes"&& location2==="no"&& location3==="yes") {
  $("#locationOne").toggle();
- }else{
+ }else if(location2==="no"&& location3==="yes"){
   $("#location2").show();
-
+  $("#location3").show();
 
  }
 
