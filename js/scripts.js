2cf77726b7c1163b82f94a1a05073330883cfb9e
Russvetsper <russvetsper@gmail.com>
2016-06-24 16:16:32 -0700

    add pipes instead and

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ var location1 = $("input:radio[name=location1]:checked").val();
 var location2 = $("input:radio[name=location2]:checked").val();
 var location3 = $("input:radio[name=location3]:checked").val();
 var location4 = $("input:radio[name=location4]:checked").val();
- if (location1 === "yes"&& location2==="no"&& location3==="yes") {
+ if (location1 === "yes" || location2==="no" ||location3==="yes") {
  $("#locationOne").toggle();
- }else if(location2==="no"&& location3==="yes"){
+ }else if(location2==="no" || location3==="yes"){
   $("#location2").show();
   $("#location3").show();
 
